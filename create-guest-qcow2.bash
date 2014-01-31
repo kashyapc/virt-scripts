b257cb7c7e8bc0ab330e801f241a0c63e06a8db1
Kashyap Chamarthy <kchamart@redhat.com>
2014-01-31 21:30:18 +0530

    Cleanup the script (thanks PJP ppandit@redhat.com)
    Update the script to start using virtio console
    by default. So, add --console pty to virt-install
    command-line. Refer rhbz#1059220

--- create-guest-qcow2.bash
+++ create-guest-qcow2.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
-# 
-# Copyright (C) 2013 Red Hat Inc.
+
+#set -x
+
+# Copyright (C) 2014 Red Hat Inc.
+# Author <kchamart@redhat.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,65 +19,37 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-#--------------------------------------------
-# Script to auto create/install unattended Virtual Machines (with bridging)
-# 1)Note: Bridging should be configured on the host to allow guests to be in the same subnet as hosts
-# 2)Creting a bridge, refer this: http://wiki.libvirt.org/page/Networking
-# 3)The kickstart file contains minimal fedora pkgs like core and text internet
-# 4)Also adds a serial console
 
-#Check if bridging is configured
-show_bridge=`brctl show | awk 'NR==2 {print $1}'`
-if [ $? -ne 0 ] ; then
-	echo "Bridged Networking is not configured, please do so to get an IP similar to your host."
-	exit 255
-fi 
-
-#check if no. of arguments are 3
-if [ "$#" != 3 ]; then
-	echo " Please provide the args according to usage"
-	echo ""
-	echo " usage: "$0 name distro arch" "
-	echo ""
-        echo "     where 'name'= f19vm1 [OR] el7vm2"
-        echo "     where 'distro' = rhel7 [OR] rhel6 [OR] f19"
-        echo "     where 'arch'   = x86_64 [OR] i386"
-	echo ""
-	exit 255
-fi
-
-# check if /export/vmimgs dir exists
-#if [ ! -d /export/vmimgs ] ; then
-#	echo "'/export/vmimgs' directory does not exist(to store vm images). Please create that."
-#	exit 255
-#fi
-
-name=$1
-distro=$2
-arch=$3
+# Script to create/install unattended Virtual Machines (with bridging)
+#
+# 1 Note: Bridging should be configured on the host to allow guests to be
+#   in the same subnet as hosts
+# 2 Creting a bridge, refer this: http://wiki.libvirt.org/page/Networking
+# 3 The kickstart file contains minimal fedora pkgs like core and text internet
+# 4 Also adds a serial console
+#
 
 
-location1=http://foo.bar.redhat.com/pub/rhel/nightly/latest-RHEL-7/compose/Server/x86_64/os/
-location2=http://foo.bar.redhat.com/pub/rhel/released/RHEL-6/6.4/Server/$arch/os
-location3=http://dl.fedoraproject.org/pub/fedora/linux/releases/19/Fedora/$arch/os/
+IMAGE_HOME="/var/lib/libvirt/images"
 
-echo "Creating domain $name..." 
-echo "Disk image will be created as  /var/lib/libvirt/images/$name.qcow2"
-
-#Disk Image location
-diskimage=/var/lib/libvirt/images/$name.qcow2
-#diskimage=/export/vmimgs2/$name.qcow2
-
-#Create the qcow2 disk image with preallocation and 'fallocate'(which pre-allocates all the blocks to a file) it for max. performance
-echo "Creating qcow2 disk image.."
-qemu-img create -f qcow2 -o preallocation=metadata $diskimage 10G
-#fallocate -l `ls -al $diskimage | awk '{print $5}'` $diskimage
-echo `ls -lash $diskimage`
+burl="http://dl.fedoraproject.org/pub"
+location1="$burl/fedora/linux/releases/19/Fedora/ARCH/os"
+location2="$burl/fedora/linux/releases/20/Fedora/ARCH/os"
 
 
-if [ "$distro" = rhel7 ]; then
-#Create the minimal kickstart file for RHEL
-cat << EOF > rhel.ks
+# External F20 mirror
+# http://dl.fedoraproject.org/pub/fedora/linux/releases/20/Fedora/x86_64/
+
+
+# Create a minimal kickstart file and return the temporary file name.
+# Do remember to delete this temporary file when it is no longer required.
+#
+create_ks_file()
+{
+    dist=$1
+    fkstart=$(mktemp -u --tmpdir=$(pwd) .XXXXXXXXXXXXXX)
+
+    cat << EOF > $fkstart
 install
 text
 reboot
@@ -94,113 +69,100 @@ autopart
 @core
 %end
 EOF
+    echo "$fkstart"
+}
 
+create_guest()
+{
+    name=$1
+    arch=$2
+    dist=$3
+    locn=$4
+    dimg=$5
+    fkst=$(create_ks_file $dist)
+    bnam=$(basename $fkst)
 
-#Create the guest
-virt-install --connect=qemu:///system \
-    --network=bridge:br0 \
-    --initrd-inject=./rhel.ks \
-    --extra-args="ks=file:/rhel.ks console=tty0 console=ttyS0,115200" \
-    --name=$name \
-    --disk path=$diskimage,format=qcow2,cache=none \
-    --ram 2048 \
-    --vcpus=2 \
-    --check-cpu \
-    --accelerate \
-    --cpuset auto \
-    --os-type linux \
-    --os-variant rhel7 \
-    --hvm \
-    --location=$location1 \
-    --nographics 
+    echo "Creating domain $name..."
+    echo "Disk image will be created at: $dimg"
 
-exit 255
-#########################################################################
+    # Create the qcow2 disk image with preallocation and 'fallocate'
+    # (which pre-allocates all the blocks to a file) it for maximum
+    # performance.
+    #
+    #   fallocate -l `ls -al $diskimage | awk '{print $5}'` $diskimage
+    #
+    echo "Creating qcow2 disk image..."
+    qemu-img create -f qcow2 -o preallocation=metadata $dimg 10G
+    echo `ls -lash $dimg`
 
-elif [ "$distro" = rhel6 ]; then
-#Create the minimal kickstart file for RHEL
-cat << EOF > rhel.ks
-install
-text
-reboot
-lang en_US.UTF-8
-keyboard us
-network --bootproto dhcp
-rootpw testpwd
-firewall --enabled --ssh
-selinux --enforcing
-timezone --utc America/New_York
-bootloader --location=mbr --append="console=tty0 console=ttyS0,115200 rd_NO_PLYMOUTH"
-zerombr
-key --skip
-clearpart --all --initlabel
-autopart
-
-%packages
-@core
-EOF
-
-
-#Create the guest
-virt-install --connect=qemu:///system \
+    virt-install --connect=qemu:///system \
     --network=bridge:virbr0 \
-    --initrd-inject=./rhel.ks \
-    --extra-args="ks=file:/rhel.ks console=tty0 console=ttyS0,115200" \
+    --initrd-inject=$bnam \
+    --extra-args="ks=file:/$bnam console=tty0 console=ttyS0,115200" \
     --name=$name \
-    --disk path=$diskimage,format=qcow2,cache=none \
+    --disk path=$dimg,format=qcow2,cache=none \
     --ram 2048 \
     --vcpus=2 \
     --check-cpu \
     --accelerate \
     --cpuset auto \
     --os-type linux \
-    --os-variant rhel6 \
+    --os-variant $dist \
     --hvm \
-    --location=$location2 \
-    --nographics
+    --location=$locn \
+    --nographics \
+    --console=pty
 
-exit 255
-#########################################################################
+    rm $fkst
+    return 0
+}
+
+# main
+{
+    # check if min no. of arguments are 3
+    #
+    if [ "$#" != 3 ]; then
+        echo -e "Usage: $0 vm-name distro arch\n"
+        echo -e "\tdistro: f19 f20"
+        echo -e "\t  arch: i386, x86_64
+        e.g. ./`basename $0` f20vm1 f20 x86_64         # create fedora 20 VM
+             ./`basename $0` f19vm1 f19 x86_64         # create fedora f19 VM"
+
+        exit 255
+    fi
+
+    # check if Linux bridging is configured
+    #
+    show_bridge=`brctl show | awk 'NR==2 {print $1}'`
+    if [ $? -ne 0 ] ; then
+        echo "Bridged Networking is not configured. " \
+             "please do so if your guest needs an IP similar to your host."
+        exit 255
+    fi
+
+    name=$1
+    dist=$2
+    arch=$3
+    dimg="$IMAGE_HOME/$name.qcow2"
+
+    locn=""
+    case "$dist" in
+        f19)
+            dist="fedora19"
+            locn=${location1/ARCH/$arch}
+            ;;
+
+        f20)
+            dist="fedora20"
+            locn=${location2/ARCH/$arch}
+            ;;
 
 
-elif [ "$distro" = f19 ]; then
-#Create Minimal kickstart file for Fedora
-cat << EOF > fed.ks
-install
-text
-reboot
-lang en_US.UTF-8
-keyboard us
-network --bootproto dhcp
-rootpw testpwd
-firewall --enabled --ssh
-selinux --enforcing
-timezone --utc America/New_York
-bootloader --location=mbr --append="console=tty0 console=ttyS0,115200 rd_NO_PLYMOUTH"
-zerombr
-clearpart --all --initlabel
-autopart
+            *)
+            echo "$0: invalid distribution name"
+            exit 255
+    esac
+    create_guest $name $arch $dist $locn $dimg
 
-%packages
-@core
-%end
-EOF
-
-# Run the VM
-virt-install --connect=qemu:///system \
-    --network=network:default \
-    --initrd-inject=./fed.ks \
-    --extra-args="ks=file:/fed.ks console=tty0 console=ttyS0,115200 serial rd_NO_PLYMOUTH" \
-    --name=$name \
-    --disk path=$diskimage,format=qcow2,cache=none \
-    --ram 2048 \
-    --vcpus=2 \
-    --check-cpu \
-    --accelerate \
-    --os-type linux \
-    --os-variant fedora17 \
-    --cpuset auto \
-    --hvm \
-    --location=$location3 \
-    --nographics 
-fi
+    exit 0
+}
