5aab71b4fcca0cec34c5ba98669cc81a62ff8583
Kashyap Chamarthy <kashyapc@fedoraproject.org>
2014-06-24 18:55:00 +0530

    Update oz-jeos.bash to create Fedora20 guests

--- oz/oz-jeos.bash
+++ oz/oz-jeos.bash
@@ -1,15 +1,32 @@
 #!/bin/bash
-# A script to create joes(just enough operating system) 
-# NOTE: once the oz-install is finished, define the xml file and start the guest
-# Author - Kashyap Chamarthy <kchamart@redhat.com>
+# A script to create JEOS(Just Enough Operating System) 
+#
+# NOTE: Once the `oz-install` command is finished, define the xml file
+# and start the guest
+#
+# Author - Kashyap Chamarthy <kchamart@redhat.com> 
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA.
+ 
 
-# usage
+# Usage
 	if [ $# -ne 2 ]; then
 		echo "Usage: $0 <guest-name> <distro>
-        'distro': f19, f18, rhel6
-        Examples: `basename $0` f19-jeos f19       # create f19
-                  `basename $0` f18-jeos f18       # create f18
-                  `basename $0` rhel6x-jeos rhel6  # create latest rhel6.x"
+        'distro': f20, f19
+        Examples: `basename $0` f20-jeos f20       # create f20 guest"
 		exit 1
 	fi
 
@@ -34,10 +51,11 @@ cat /proc/sys/net/ipv4/ip_forward
 		echo "IP forwarding enabled"
 	fi
 
-# A few checks so that oz-install won't go crazy with iptables. (Just deals with the default virt-network)
-run_command() {
-    echo "# $@"
-    eval "$@"
+# A few checks so that oz-install won't go crazy with iptables. (Just
+# deals with the default virt-network)
+run_command() { 
+    echo "# $@" 
+    eval "$@" 
 }
 
 (
@@ -47,52 +65,27 @@ run_command /usr/bin/virsh net-list --all
 run_command cat /proc/sys/net/ipv4/ip_forward
 ) >& $LOGFILE2
 
-if [ "$distro" = rhel6 ]; then
-#create the tdl file(note:the below url will be automatically redirected to our local tree))
-function _make_tdl_rhel()  {
-cat << EOF > $TDLFILE
-<template>
-  <name>$NAME</name>
-  <os>
-    <name>RHEL-6</name>
-    <version>4</version>
-    <arch>x86_64</arch>
-    <install type='url'>
-      <url>http://download.foo.bar.redhat.com/pub/rhel/released/RHEL-6/6.4/Server/x86_64/os</url>
-    </install>
-    <rootpw>redhat</rootpw>
-  </os>
-  <description>RHEL 6.4 </description>
-</template>
+if [ "$distro" = f20 ]; then
 
-EOF
-}
-
-# Create the TDL file
-_make_tdl_rhel
-
-#exit 255
-
-elif [ "$distro" = f18 ]; then
-
+# Create the tdl file(note:the below url will be automatically redirected
+# to our local tree))
 function _make_tdl_fed()  {
 cat << EOF > $TDLFILE
 <template>
   <name>$NAME</name>
   <os>
     <name>Fedora</name>
-    <version>18</version>
+    <version>4</version>
     <arch>x86_64</arch>
     <install type='url'>
-      <url>http://dl.fedoraproject.org/pub/fedora/linux/releases/18/Fedora/x86_64/os/</url>
+      <url>http://dl.fedoraproject.org/pub/fedora/linux/releases/20/Fedora/x86_64/os</url>
     </install>
-    <rootpw>redhat</rootpw>
+    <rootpw>fedora</rootpw>
   </os>
-  <description>Fedora 18</description>
+  <description>Fedora 20</description>
 </template>
 EOF
 }
-
 
 elif [ "$distro" = f19 ]; then
 
@@ -102,14 +95,14 @@ cat << EOF > $TDLFILE
   <name>$NAME</name>
   <os>
     <name>Fedora</name>
-    <version>18</version>
+    <version>19</version>
     <arch>x86_64</arch>
     <install type='url'>
       <url>http://dl.fedoraproject.org/pub/fedora/linux/releases/19/Fedora/x86_64/os/</url>
     </install>
-    <rootpw>redhat</rootpw>
+    <rootpw>fedora</rootpw>
   </os>
-  <description>Fedora 18</description>
+  <description>Fedora 19</description>
 </template>
 EOF
 }
