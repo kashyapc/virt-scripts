05f406670414c3cd4f76d7a5f499a0d3de2bf889
Kashyap Chamarthy <kchamart@redhat.com>
2016-03-17 06:31:08 +0100

    Update README and Oz script

--- oz/oz-jeos.bash
+++ oz/oz-jeos.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
-# A script to create JEOS(Just Enough Operating System) 
 #
+# Purpose: A script to create JEOS (Just Enough Operating System) guest
+#
+# Copyright (C) 2015-2016 Red Hat Inc.
 # NOTE: Once the `oz-install` command is finished, define the xml file
 # and start the guest
 #
@@ -21,12 +23,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301 USA.
  
+set -x
 
 # Usage
 	if [ $# -ne 2 ]; then
 		echo "Usage: $0 <guest-name> <distro>
-        'distro': f20, f19
-        Examples: `basename $0` f20-jeos f20       # create f20 guest"
+        'distro': f23, f22
+        Examples: `basename $0` f23-vm1 f23       # Create a F23 guest"
 		exit 1
 	fi
 
@@ -51,8 +54,9 @@ cat /proc/sys/net/ipv4/ip_forward
 		echo "IP forwarding enabled"
 	fi
 
-# A few checks so that oz-install won't go crazy with iptables. (Just
-# deals with the default virt-network)
+# A few checks so that `oz-install` won't go crazy with iptables. (Just
+# deals with the default libvirt network)
+
 run_command() { 
     echo "# $@" 
     eval "$@" 
@@ -65,29 +69,9 @@ run_command /usr/bin/virsh net-list --all
 run_command cat /proc/sys/net/ipv4/ip_forward
 ) >& $LOGFILE2
 
-if [ "$distro" = f20 ]; then
+if [ "$distro" = f23 ]; then
 
-# Create the tdl file(note:the below url will be automatically redirected
-# to our local tree))
-function _make_tdl_fed()  {
-cat << EOF > $TDLFILE
-<template>
-  <name>$NAME</name>
-  <os>
-    <name>Fedora</name>
-    <version>4</version>
-    <arch>x86_64</arch>
-    <install type='url'>
-      <url>http://dl.fedoraproject.org/pub/fedora/linux/releases/20/Fedora/x86_64/os</url>
-    </install>
-    <rootpw>fedora</rootpw>
-  </os>
-  <description>Fedora 20</description>
-</template>
-EOF
-}
-
-elif [ "$distro" = f19 ]; then
+# Create the TDL (Template Description Language) file
 
 function _make_tdl_fed()  {
 cat << EOF > $TDLFILE
@@ -95,24 +79,43 @@ cat << EOF > $TDLFILE
   <name>$NAME</name>
   <os>
     <name>Fedora</name>
-    <version>19</version>
+    <version>23</version>
     <arch>x86_64</arch>
     <install type='url'>
-      <url>http://dl.fedoraproject.org/pub/fedora/linux/releases/19/Fedora/x86_64/os/</url>
+      <url>http://dl.fedoraproject.org/pub/fedora/linux/releases/23/Server/x86_64/os</url>
     </install>
     <rootpw>fedora</rootpw>
   </os>
-  <description>Fedora 19</description>
+  <description>Fedora 23</description>
 </template>
 EOF
 }
+
+elif [ "$distro" = f22 ]; then
+
+function _make_tdl_fed()  {
+cat << EOF > $TDLFILE
+<template>
+  <name>$NAME</name>
+  <os>
+    <name>Fedora</name>
+    <version>f22</version>
+    <arch>x86_64</arch>
+    <install type='url'>
+      <url>http://dl.fedoraproject.org/pub/fedora/linux/releases/22/Fedora/x86_64/os/</url>
+    </install>
+    <rootpw>fedora</rootpw>
+  </os>
+  <description>Fedora 22</description>
+</template>
+EOF
+}
+
+fi
 
 # Create the TDL file
 _make_tdl_fed
-
-fi
 sleep 2
 
-#run the oz script as root
+# Invoke `oz-install`, as root
 /usr/bin/oz-install -d 4 $TDLFILE 2>&1 | tee $LOGFILE
-
